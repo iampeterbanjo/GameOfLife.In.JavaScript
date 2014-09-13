699801ebec57cfdcce99bf0fe29ee9cb8ac2bd41
iampeterbanjo <me@iampeterbanjo.com>
2014-09-14 00:20:09 +0100

    add grid spawning at valid locations

--- game.of.life-spec.js
+++ game.of.life-spec.js
@@ -9,9 +9,27 @@ describe("the game of life", function () {
 		it("should have a width and height", function () {
 			var w = 10
 					, h = 20
-					, grid = new Grid(w, h)
+					, grid = new Grid(h, w)
 			expect(grid.width).toEqual(w)
 			expect(grid.height).toEqual(h)
+		})
+
+		it("should allow positions on the grid to be spawned", function () {
+			expect(new Grid().spawn).toBeDefined()
+		})
+
+		it("should spawn at a location", function () {
+			var grid = new Grid(10, 20)
+			grid.spawn(1, 2) // y, x
+			expect(grid.isAlive(1, 2)).toBe(true)
+		})
+
+		it("should spawn only at valid locations", function () {
+			var grid = new Grid(2, 2)
+			grid.spawn(1, 1)
+			grid.spawn(5, 4)
+			expect(grid.isAlive(1, 1)).toBe(true)
+			expect(grid.isAlive(5, 4)).toBe(false)
 		})
 	})
 })
--- game.of.life.js
+++ game.of.life.js
@@ -1,10 +1,30 @@
 (function () {
-	var _ = self.Grid = function (width, height) {
-		this.width = width
-		this.height = height
+	// Grid defines the 2D world in which the game
+	// plays out
+	var _ = self.Grid = function (height, width) {
+		var rows = new Array(height)
+
+		for (var i = 0; i < rows.length; i++) {
+			rows[i] = new Array(width)
+		}
+		this._grid = rows
+		this.width = width // columns
+		this.height = height // rows
 	}
 
 	_.prototype = {
-
+		// create life on the grid
+		spawn: function (y, x) {
+			if (y < this.height && x < this.width) {
+				this._grid[y][x] = true
+			}
+		}
+		, isAlive: function (y, x) {
+			var alive = false
+			if (y < this.height && x < this.width) {
+				alive = this._grid[y][x]
+			}
+			return alive
+		}
 	}
 })()
