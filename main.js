5ee89bb2bd45034221a7fa44986f8665d1e209c6
iampeterbanjo <peterbanjo@gmail.com>
2016-01-25 10:26:57 +0000

    Refactor JS interaction into a GameView object

--- main.js
+++ main.js
@@ -1,49 +1,71 @@
+(function() {
+	var _ = self.GameView = function(args) {
+		this.args = args
+		
+		return {
+			init: function() {
+				_.game = new Game(args.width, args.height)
+				this.view = document.querySelector(args.selector)
+				this.update()
+				this.watchNext()
+				this.watchGridChange()
+				this.watchAutoplay()
+			}
+			, update: function() {
+				this.view.innerHTML = ''
+				this.view.appendChild(_.game.draw())
+			}
+			, next: function() {
+				_.game.next()
+				this.update()
+			}
+			, watchNext: function() {
+				var me = this
+				document.querySelector('.next').addEventListener('click', function() {
+					_.game.next()
+					me.update()
+				})
+			}
+			, watchGridChange: function () {
+			this.view.addEventListener('change', function(event) {
+				var input = event.target
+						, x = input.getAttribute('data-x')
+						, y = input.getAttribute('data-y')
+						, checked = input.checked
+
+				if(checked) {
+					_.game.spawn(x,y)
+				} else {
+					_.game.kill(x,y)
+				}
+			})
+		}
+		, watchAutoplay: function() {
+				var me = this
+				var autoplay = document.querySelector('.autoplay')
+						, interval
+				autoplay.addEventListener('change', function (event) {
+					var input = event.target
+							, checked = input.checked
+					
+					if(checked) {
+						interval = setInterval(function(){
+							_.game.next()
+							me.update()
+						}, 500)
+					} else {
+						clearInterval(interval)
+					}
+				})
+			}
+		}
+	};
+	
+})();
 
 document.addEventListener('DOMContentLoaded', function() {
 	console.log('ready');
 	
-	var grid = new Grid(5, 5)
-			, gridWrapper = document.querySelector('#grid-wrapper')
-
-	function update() {
-		gridWrapper.innerHTML = ''
-		gridWrapper.appendChild(grid.draw())
-	}
-	
-	update()
-	
-	var next = document.querySelector('.next')
-	next.addEventListener('click', function() {
-		grid.next()
-		update()
-	})
-	
-	gridWrapper.addEventListener('change', function(event) {
-		var input = event.target
-				, x = input.getAttribute('data-x')
-				, y = input.getAttribute('data-y')
-				, checked = input.checked
-
-		if(checked) {
-			grid.spawn(x,y)
-		} else {
-			grid.kill(x,y)
-		}
-	})
-	
-	var autoplay = document.querySelector('.autoplay')
-			, interval
-	autoplay.addEventListener('change', function (event) {
-		var input = event.target
-				, checked = input.checked
-		
-		if(checked) {
-			interval = setInterval(function(){
-				grid.next()
-				update()
-			}, 500)
-		} else {
-			clearInterval(interval)
-		}
-	})
+	var gameView = new GameView({width: 5, height: 5, selector: '#grid-wrapper'})
+	gameView.init()
 })
